06307d9c6f1d228e4dbab02f69c86cef791ace17
orhun <orhunparmaksiz@gmail.com>
2020-02-19 19:50:12 +0300

    feat: Add collisions for jumping

--- KBodySinasi.gd
+++ KBodySinasi.gd
@@ -1,12 +1,22 @@
 extends KinematicBody2D
+const UP = Vector2(0, -1)
+const GRAVITY = 30
+const SPEED = 200
+const JUMP_HEIGHT = -300
+
 var motion = Vector2()
 
 func _physics_process(delta):
-	motion.y += 10
+
+	motion.y += GRAVITY
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 300
+		motion.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -300
+		motion.x = -SPEED
 	else:
 		motion.x = 0
-	move_and_slide(motion)
+
+	if Input.is_action_pressed("ui_accept"):
+		motion.y = JUMP_HEIGHT
+
+	motion = move_and_slide(motion, UP)
