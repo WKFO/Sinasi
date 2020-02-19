af1a2901951321d989e9f9711b8e1a9c1f03a6f8
orhun <orhunparmaksiz@gmail.com>
2020-02-19 19:04:03 +0300

    feat: Add left and right movement to the test body

--- KBodySinasi.gd
+++ KBodySinasi.gd
@@ -1,16 +1,11 @@
 extends KinematicBody2D
+var motion = Vector2()
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		motion.x = 300
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -300
+	else:
+		motion.x = 0
+	move_and_slide(motion)
