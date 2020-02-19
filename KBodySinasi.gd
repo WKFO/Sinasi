90124888c81fd1b88e550b6675f46206c7b01f7a
orhun <orhunparmaksiz@gmail.com>
2020-02-19 19:08:50 +0300

    refactor: Rename the parent test node

--- KBodySinasi.gd
+++ KBodySinasi.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 var motion = Vector2()
 
 func _physics_process(delta):
+	motion.y += 10
 	if Input.is_action_pressed("ui_right"):
 		motion.x = 300
 	elif Input.is_action_pressed("ui_left"):
